026ca0213b9d8d2347461695dc7651304a50bc60
UKTransport <ibrahimjutt@hotmail.com>
2019-11-03 23:50:30 -0500

    cc
    cc

---
+++ pages/script.js
@@ -0,0 +1,17 @@
+function greeting(){
+	var name= prompt("Whats your name");
+	alert(" hello "+name);
+	// body...
+}
+
+
+
+
+
+function thirdangle() {
+let angle1 = prompt("give 1st angle ");
+let angle2 = prompt("give 2nd angle ");
+let angle3 = 180 - angle1-angle2;
+alert ("the 3rd angle is"+ angle3);
+
+}
