942f93d319d3ef957fcfa634d49a427217b60920
UKTransport <ibrahimjutt@hotmail.com>
2019-11-04 10:10:47 -0500

    m
    n

--- pages/script.js
+++ pages/script.js
@@ -6,12 +6,22 @@ function greeting(){
 
 
 
-
-
-function thirdangle() {
+function thirdAngle() {
 let angle1 = prompt("give 1st angle ");
 let angle2 = prompt("give 2nd angle ");
 let angle3 = 180 - angle1-angle2;
 alert ("the 3rd angle is"+ angle3);
 
 }
+
+
+
+function centuryFromYear(year) {
+	  var x = Math.floor(year/100);
+	  if (year%100===0) {
+	  	 alert( x);
+
+	}
+	else {
+		alert( x + 1);
+	}
